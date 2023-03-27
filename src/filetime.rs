4fb796e49b5ce3c9eb8d1cee40567cb4b22e87f9
Shun Sakai <sorairolake@protonmail.ch>
2023-03-27 17:37:56 +0900

    Add methods for operation

--- src/filetime.rs
+++ src/filetime.rs
@@ -8,7 +8,10 @@
 //!
 //! [file-time-docs-url]: https://docs.microsoft.com/en-us/windows/win32/sysinfo/file-times
 
-use core::fmt;
+use core::{
+    fmt,
+    ops::{Add, AddAssign, Sub, SubAssign},
+};
 
 use time::{macros::datetime, OffsetDateTime};
 
@@ -91,6 +94,126 @@ impl FileTime {
     pub const fn as_u64(self) -> u64 {
         self.0
     }
+
+    /// Computes `self + duration`, returning [`None`] if overflow occurred.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use core::time::Duration;
+    /// #
+    /// # use nt_time::FileTime;
+    /// #
+    /// assert_eq!(
+    ///     FileTime::NT_EPOCH.checked_add(Duration::from_nanos(1)),
+    ///     Some(FileTime::NT_EPOCH)
+    /// );
+    /// assert_eq!(
+    ///     FileTime::NT_EPOCH.checked_add(Duration::from_nanos(100)),
+    ///     Some(FileTime::new(1))
+    /// );
+    ///
+    /// assert_eq!(FileTime::MAX.checked_add(Duration::from_nanos(100)), None);
+    /// ```
+    #[must_use]
+    pub fn checked_add(self, duration: core::time::Duration) -> Option<Self> {
+        let duration = u64::try_from(duration.as_nanos() / 100).ok()?;
+        let ft = self.as_u64().checked_add(duration)?;
+        Some(Self::new(ft))
+    }
+
+    /// Computes `self - duration`, returning [`None`] if the result would be
+    /// negative or if overflow occurred.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use core::time::Duration;
+    /// #
+    /// # use nt_time::FileTime;
+    /// #
+    /// assert_eq!(
+    ///     FileTime::MAX.checked_sub(Duration::from_nanos(1)),
+    ///     Some(FileTime::MAX)
+    /// );
+    /// assert_eq!(
+    ///     FileTime::MAX.checked_sub(Duration::from_nanos(100)),
+    ///     Some(FileTime::new(u64::MAX - 1))
+    /// );
+    ///
+    /// assert_eq!(
+    ///     FileTime::NT_EPOCH.checked_sub(Duration::from_nanos(100)),
+    ///     None
+    /// );
+    /// ```
+    #[must_use]
+    pub fn checked_sub(self, duration: core::time::Duration) -> Option<Self> {
+        let duration = u64::try_from(duration.as_nanos() / 100).ok()?;
+        let ft = self.as_u64().checked_sub(duration)?;
+        Some(Self::new(ft))
+    }
+
+    /// Computes `self + duration`, returning [`FileTime::MAX`] if overflow
+    /// occurred.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use core::time::Duration;
+    /// #
+    /// # use nt_time::FileTime;
+    /// #
+    /// assert_eq!(
+    ///     FileTime::NT_EPOCH.saturating_add(Duration::from_nanos(1)),
+    ///     FileTime::NT_EPOCH
+    /// );
+    /// assert_eq!(
+    ///     FileTime::NT_EPOCH.saturating_add(Duration::from_nanos(100)),
+    ///     FileTime::new(1)
+    /// );
+    ///
+    /// assert_eq!(
+    ///     FileTime::MAX.saturating_add(Duration::from_nanos(100)),
+    ///     FileTime::MAX
+    /// );
+    /// ```
+    #[must_use]
+    pub fn saturating_add(self, duration: core::time::Duration) -> Self {
+        let duration = u64::try_from(duration.as_nanos() / 100).unwrap_or(u64::MAX);
+        let ft = self.as_u64().saturating_add(duration);
+        Self::new(ft)
+    }
+
+    /// Computes `self - duration`, returning [`FileTime::NT_EPOCH`] if the
+    /// result would be negative or if overflow occurred.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use core::time::Duration;
+    /// #
+    /// # use nt_time::FileTime;
+    /// #
+    /// assert_eq!(
+    ///     FileTime::MAX.saturating_sub(Duration::from_nanos(1)),
+    ///     FileTime::MAX
+    /// );
+    /// assert_eq!(
+    ///     FileTime::MAX.saturating_sub(Duration::from_nanos(100)),
+    ///     FileTime::new(u64::MAX - 1)
+    /// );
+    ///
+    /// assert_eq!(
+    ///     FileTime::NT_EPOCH.saturating_sub(Duration::from_nanos(100)),
+    ///     FileTime::NT_EPOCH
+    /// );
+    /// ```
+    #[must_use]
+    pub fn saturating_sub(self, duration: core::time::Duration) -> Self {
+        let duration = u64::try_from(duration.as_nanos() / 100).unwrap_or(u64::MAX);
+        let ft = self.as_u64().saturating_sub(duration);
+        Self::new(ft)
+    }
 }
 
 impl Default for FileTime {
@@ -113,6 +236,55 @@ impl fmt::Display for FileTime {
     #[inline]
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         u64::from(*self).fmt(f)
+    }
+}
+
+impl Add<core::time::Duration> for FileTime {
+    type Output = Self;
+
+    fn add(self, rhs: core::time::Duration) -> Self::Output {
+        let ft = self.as_u64()
+            + u64::try_from(rhs.as_nanos() / 100)
+                .expect("duration should be in the range of `u64`");
+        Self::new(ft)
+    }
+}
+
+impl AddAssign<core::time::Duration> for FileTime {
+    fn add_assign(&mut self, rhs: core::time::Duration) {
+        let ft = self.as_u64()
+            + u64::try_from(rhs.as_nanos() / 100)
+                .expect("duration should be in the range of `u64`");
+        *self = Self::new(ft);
+    }
+}
+
+impl Sub for FileTime {
+    type Output = core::time::Duration;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        let duration = (self.as_u64() - rhs.as_u64()) * 100;
+        Self::Output::from_nanos(duration)
+    }
+}
+
+impl Sub<core::time::Duration> for FileTime {
+    type Output = Self;
+
+    fn sub(self, rhs: core::time::Duration) -> Self::Output {
+        let ft = self.as_u64()
+            - u64::try_from(rhs.as_nanos() / 100)
+                .expect("duration should be in the range of `u64`");
+        Self::new(ft)
+    }
+}
+
+impl SubAssign<core::time::Duration> for FileTime {
+    fn sub_assign(&mut self, rhs: core::time::Duration) {
+        let ft = self.as_u64()
+            - u64::try_from(rhs.as_nanos() / 100)
+                .expect("duration should be in the range of `u64`");
+        *self = Self::new(ft);
     }
 }
 
@@ -329,6 +501,127 @@ mod tests {
     }
 
     #[test]
+    fn checked_add() {
+        use core::time::Duration;
+
+        assert_eq!(
+            FileTime::NT_EPOCH.checked_add(Duration::from_nanos(1)),
+            Some(FileTime::NT_EPOCH)
+        );
+        assert_eq!(
+            FileTime::NT_EPOCH.checked_add(Duration::from_nanos(99)),
+            Some(FileTime::NT_EPOCH)
+        );
+        assert_eq!(
+            FileTime::NT_EPOCH.checked_add(Duration::from_nanos(100)),
+            Some(FileTime::new(1))
+        );
+
+        assert_eq!(
+            FileTime::MAX.checked_add(Duration::from_nanos(1)),
+            Some(FileTime::MAX)
+        );
+        assert_eq!(
+            FileTime::MAX.checked_add(Duration::from_nanos(99)),
+            Some(FileTime::MAX)
+        );
+        assert_eq!(FileTime::MAX.checked_add(Duration::from_nanos(100)), None);
+    }
+
+    #[test]
+    fn checked_sub() {
+        use core::time::Duration;
+
+        assert_eq!(
+            FileTime::MAX.checked_sub(Duration::from_nanos(1)),
+            Some(FileTime::MAX)
+        );
+        assert_eq!(
+            FileTime::MAX.checked_sub(Duration::from_nanos(99)),
+            Some(FileTime::MAX)
+        );
+        assert_eq!(
+            FileTime::MAX.checked_sub(Duration::from_nanos(100)),
+            Some(FileTime::new(u64::MAX - 1))
+        );
+
+        assert_eq!(
+            FileTime::NT_EPOCH.checked_sub(Duration::from_nanos(1)),
+            Some(FileTime::NT_EPOCH)
+        );
+        assert_eq!(
+            FileTime::NT_EPOCH.checked_sub(Duration::from_nanos(99)),
+            Some(FileTime::NT_EPOCH)
+        );
+        assert_eq!(
+            FileTime::NT_EPOCH.checked_sub(Duration::from_nanos(100)),
+            None
+        );
+    }
+
+    #[test]
+    fn saturating_add() {
+        use core::time::Duration;
+
+        assert_eq!(
+            FileTime::NT_EPOCH.saturating_add(Duration::from_nanos(1)),
+            FileTime::NT_EPOCH
+        );
+        assert_eq!(
+            FileTime::NT_EPOCH.saturating_add(Duration::from_nanos(99)),
+            FileTime::NT_EPOCH
+        );
+        assert_eq!(
+            FileTime::NT_EPOCH.saturating_add(Duration::from_nanos(100)),
+            FileTime::new(1)
+        );
+
+        assert_eq!(
+            FileTime::MAX.saturating_add(Duration::from_nanos(1)),
+            FileTime::MAX
+        );
+        assert_eq!(
+            FileTime::MAX.saturating_add(Duration::from_nanos(99)),
+            FileTime::MAX
+        );
+        assert_eq!(
+            FileTime::MAX.saturating_add(Duration::from_nanos(100)),
+            FileTime::MAX
+        );
+    }
+
+    #[test]
+    fn saturating_sub() {
+        use core::time::Duration;
+
+        assert_eq!(
+            FileTime::MAX.saturating_sub(Duration::from_nanos(1)),
+            FileTime::MAX
+        );
+        assert_eq!(
+            FileTime::MAX.saturating_sub(Duration::from_nanos(99)),
+            FileTime::MAX
+        );
+        assert_eq!(
+            FileTime::MAX.saturating_sub(Duration::from_nanos(100)),
+            FileTime::new(u64::MAX - 1)
+        );
+
+        assert_eq!(
+            FileTime::NT_EPOCH.saturating_sub(Duration::from_nanos(1)),
+            FileTime::NT_EPOCH
+        );
+        assert_eq!(
+            FileTime::NT_EPOCH.saturating_sub(Duration::from_nanos(99)),
+            FileTime::NT_EPOCH
+        );
+        assert_eq!(
+            FileTime::NT_EPOCH.saturating_sub(Duration::from_nanos(100)),
+            FileTime::NT_EPOCH
+        );
+    }
+
+    #[test]
     fn default() {
         assert_eq!(FileTime::default(), FileTime::NT_EPOCH);
     }
@@ -337,6 +630,172 @@ mod tests {
     fn display() {
         assert_eq!(format!("{}", FileTime::NT_EPOCH), "0");
         assert_eq!(format!("{}", FileTime::MAX), "18446744073709551615");
+    }
+
+    #[test]
+    fn add_duration() {
+        use core::time::Duration;
+
+        assert_eq!(
+            FileTime::NT_EPOCH + Duration::from_nanos(1),
+            FileTime::NT_EPOCH
+        );
+        assert_eq!(
+            FileTime::NT_EPOCH + Duration::from_nanos(99),
+            FileTime::NT_EPOCH
+        );
+        assert_eq!(
+            FileTime::NT_EPOCH + Duration::from_nanos(100),
+            FileTime::new(1)
+        );
+
+        assert_eq!(FileTime::MAX + Duration::from_nanos(1), FileTime::MAX);
+        assert_eq!(FileTime::MAX + Duration::from_nanos(99), FileTime::MAX);
+    }
+
+    #[test]
+    #[should_panic]
+    fn add_duration_with_overflow() {
+        use core::time::Duration;
+
+        let _ = FileTime::MAX + Duration::from_nanos(100);
+    }
+
+    #[test]
+    fn add_assign_duration() {
+        use core::time::Duration;
+
+        {
+            let mut ft = FileTime::NT_EPOCH;
+            ft += Duration::from_nanos(1);
+            assert_eq!(ft, FileTime::NT_EPOCH);
+        }
+        {
+            let mut ft = FileTime::NT_EPOCH;
+            ft += Duration::from_nanos(99);
+            assert_eq!(ft, FileTime::NT_EPOCH);
+        }
+        {
+            let mut ft = FileTime::NT_EPOCH;
+            ft += Duration::from_nanos(100);
+            assert_eq!(ft, FileTime::new(1));
+        }
+
+        {
+            let mut ft = FileTime::MAX;
+            ft += Duration::from_nanos(1);
+            assert_eq!(ft, FileTime::MAX);
+        }
+        {
+            let mut ft = FileTime::MAX;
+            ft += Duration::from_nanos(99);
+            assert_eq!(ft, FileTime::MAX);
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn add_assign_duration_with_overflow() {
+        use core::time::Duration;
+
+        let mut ft = FileTime::MAX;
+        ft += Duration::from_nanos(100);
+    }
+
+    #[test]
+    fn sub_file_time() {
+        use core::time::Duration;
+
+        assert_eq!(
+            FileTime::MAX - (FileTime::MAX - Duration::from_nanos(1)),
+            Duration::ZERO
+        );
+        assert_eq!(
+            FileTime::MAX - (FileTime::MAX - Duration::from_nanos(99)),
+            Duration::ZERO
+        );
+        assert_eq!(
+            FileTime::MAX - (FileTime::MAX - Duration::from_nanos(100)),
+            Duration::from_nanos(100)
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn sub_file_time_with_overflow() {
+        use core::time::Duration;
+
+        let _ = (FileTime::MAX - Duration::from_nanos(100)) - FileTime::MAX;
+    }
+
+    #[test]
+    fn sub_duration() {
+        use core::time::Duration;
+
+        assert_eq!(FileTime::MAX - Duration::from_nanos(1), FileTime::MAX);
+        assert_eq!(FileTime::MAX - Duration::from_nanos(99), FileTime::MAX);
+        assert_eq!(
+            FileTime::MAX - Duration::from_nanos(100),
+            FileTime::new(u64::MAX - 1)
+        );
+
+        assert_eq!(
+            FileTime::NT_EPOCH - Duration::from_nanos(1),
+            FileTime::NT_EPOCH
+        );
+        assert_eq!(
+            FileTime::NT_EPOCH - Duration::from_nanos(99),
+            FileTime::NT_EPOCH
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn sub_duration_with_overflow() {
+        use core::time::Duration;
+
+        let _ = FileTime::NT_EPOCH - Duration::from_nanos(100);
+    }
+
+    #[test]
+    fn sub_assign_duration() {
+        use core::time::Duration;
+
+        {
+            let mut ft = FileTime::MAX;
+            ft -= Duration::from_nanos(1);
+            assert_eq!(ft, FileTime::MAX);
+        }
+        {
+            let mut ft = FileTime::MAX;
+            ft -= Duration::from_nanos(99);
+            assert_eq!(ft, FileTime::MAX);
+        }
+        {
+            let mut ft = FileTime::MAX;
+            ft -= Duration::from_nanos(100);
+            assert_eq!(ft, FileTime::new(u64::MAX - 1));
+        }
+
+        {
+            let mut ft = FileTime::NT_EPOCH;
+            ft -= Duration::from_nanos(1);
+            assert_eq!(ft, FileTime::NT_EPOCH);
+        }
+        {
+            let mut ft = FileTime::NT_EPOCH;
+            ft -= Duration::from_nanos(99);
+            assert_eq!(ft, FileTime::NT_EPOCH);
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn sub_assign_duration_with_overflow() {
+        use core::time::Duration;
+
+        let mut ft = FileTime::NT_EPOCH;
+        ft -= Duration::from_nanos(100);
     }
 
     #[test]
