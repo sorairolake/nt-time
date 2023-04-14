001613ea885e454dd02be5ab4cf574a0bea70ed4
Shun Sakai <sorairolake@protonmail.ch>
2023-04-14 21:53:29 +0900

    Update version to 0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -36,7 +36,7 @@
 //! [file-time-docs-url]: https://docs.microsoft.com/en-us/windows/win32/sysinfo/file-times
 //! [7z-format-url]: https://www.7-zip.org/7z.html
 
-#![doc(html_root_url = "https://docs.rs/nt-time/0.1.0/")]
+#![doc(html_root_url = "https://docs.rs/nt-time/0.2.0/")]
 #![no_std]
 #![cfg_attr(doc_cfg, feature(doc_cfg))]
 // Lint levels of rustc.
