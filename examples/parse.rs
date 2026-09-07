eacd481fe3d0edff905b5e18aac937b32f77a2da
Shun Sakai <sorairolake@protonmail.ch>
2026-09-08 05:10:30 +0900

    Refactor DateTime struct to use Timestamp directly

--- examples/parse.rs
+++ examples/parse.rs
@@ -11,7 +11,7 @@ use clap::{Parser, ValueEnum};
 use nt_time::{
     FileTime,
     time::{
-        time::Timestamp,
+        Timestamp,
         error::Parse,
         format_description::well_known::{Iso8601, Rfc2822, Rfc3339},
     },
@@ -51,10 +51,10 @@ enum Format {
 }
 
 #[derive(Clone, Debug)]
-struct DateTime(time::Timestamp);
+struct DateTime(Timestamp);
 
 impl Deref for DateTime {
-    type Target = time::Timestamp;
+    type Target = Timestamp;
 
     fn deref(&self) -> &Self::Target {
         &self.0
@@ -65,9 +65,9 @@ impl FromStr for DateTime {
     type Err = Parse;
 
     fn from_str(dt: &str) -> Result<Self, Self::Err> {
-        time::Timestamp::parse(dt, &Iso8601::DEFAULT)
-            .or_else(|_| time::Timestamp::parse(dt, &Rfc2822))
-            .or_else(|_| time::Timestamp::parse(dt, &Rfc3339))
+        Timestamp::parse(dt, &Iso8601::DEFAULT)
+            .or_else(|_| Timestamp::parse(dt, &Rfc2822))
+            .or_else(|_| Timestamp::parse(dt, &Rfc3339))
             .map(Self)
     }
 }
